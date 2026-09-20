726ef774638fa4062752d96b51979a374a9f1e4c
Shengtuo Hu <3949029+h1994st@users.noreply.github.com>
2026-09-20 06:47:42 +0000

    rllvm 0.6.2

--- Formula/rllvm.rb
+++ Formula/rllvm.rb
@@ -1,8 +1,8 @@
 class Rllvm < Formula
   desc "Compiler wrappers for building whole-program LLVM bitcode files"
   homepage "https://shengtuo.me/rllvm/"
-  url "https://github.com/h1994st/rllvm/releases/download/rllvm-v0.6.1/source.tar.gz"
-  sha256 "93a12a508b41a731c187950c7ddbcfc233e6bff0bcdd1495cfc867c6fe8921ec"
+  url "https://github.com/h1994st/rllvm/releases/download/rllvm-v0.6.2/source.tar.gz"
+  sha256 "8fed1f19e21b7ad1e904e6aa106f4e50be5b55ba76115a4a8e0796f2e8e9b0b9"
   license "Apache-2.0"
   head "https://github.com/h1994st/rllvm.git", branch: "main"
 
